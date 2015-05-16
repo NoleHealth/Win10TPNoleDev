fb4b90c6534371b40fb51cfc6efd8c4efbdf5b5f
Nole Dev <NoleDev@outlook.com>
2015-05-16 06:11:19 -0400

    Shell hamb nav 1 working. need style and tweaks

---
+++ HelloWorld1/WipHello/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace WipHello.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void GotoAbout(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            (App.Current as Common.BootStrapper).NavigationService.Navigate(typeof(Views.AboutPage));
+        }
+    }
+}
